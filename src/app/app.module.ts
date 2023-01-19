5842dc81aa13f5866a87cfd5d6e9b0c48f55ff4f
Akash <akashchouhan806@gmail.com>
2023-01-20 03:16:11 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PropertyCardComponent } from './property-card/property-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    PropertyCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
